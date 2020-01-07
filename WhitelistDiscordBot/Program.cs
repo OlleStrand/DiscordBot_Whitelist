fd48af1ab9f457edb4640bf96ca1e5b592a13486
Olle Strand <ollestrand02@gmail.com>
2020-01-07 22:39:09 +0100

    Files

---
+++ WhitelistDiscordBot/Program.cs
@@ -0,0 +1,139 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Discord.Commands;
+using WhitelistDiscordBot.Services;
+
+namespace WhitelistDiscordBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandHandler _commandHandler;
+
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commandHandler = new CommandHandler();
+
+
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, ConfigurationManager.AppSettings["DiscordToken"].ToString());
+            await _client.StartAsync();
+            await _commandHandler.InstallCommandsAsync(_client);
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("whitelist")]
+        [Summary("Add user to whitelist DB")]
+        public async Task AddWhitelist(string hexID)
+        {
+            var user = Context.User as SocketGuildUser;
+            if (!user.GuildPermissions.Administrator)
+            {
+                var msg = await ReplyAsync("You have to be an Admin to whitelist");
+                await msg.DeleteAsync();
+
+                return;
+            }
+
+            DBConnect dBConnect = new DBConnect();
+
+            if (hexID != "" || hexID != null)
+            {
+                var msg = await ReplyAsync("Whitelisting User");
+
+                //DB Stuff
+                if (!dBConnect.InWhitelist(hexID))
+                {
+                    if (dBConnect.Whitelist(hexID))
+                    {
+                        Console.WriteLine($"{DateTime.Now.TimeOfDay}    Whitelisted user: steam:{hexID}");
+                        await ReplyAsync($"User steam:{hexID} is now whitelisted");
+                        await msg.DeleteAsync();
+                    }
+                    else
+                    {
+                        await ReplyAsync($"Could not connect to DB");
+                        await msg.DeleteAsync();
+                    }
+                }
+                else
+                {
+                    if (dBConnect.UpdateWhitelist(hexID, 1))
+                    {
+                        Console.WriteLine($"{DateTime.Now.TimeOfDay}    Whitelisted user: steam:{hexID}");
+                        await ReplyAsync($"User steam:{hexID} is now whitelisted");
+                        await msg.DeleteAsync();
+                    }
+                    else
+                    {
+                        await ReplyAsync($"Could not connect to DB");
+                        await msg.DeleteAsync();
+                    }
+                }
+            }
+        }
+
+        [Command("-whitelist")]
+        [Summary("Remove user from whitelist DB")]
+        public async Task RemoveWhitelist(string hexID)
+        {
+            var user = Context.User as SocketGuildUser;
+            if (!user.GuildPermissions.Administrator)
+            {
+                var msg = await ReplyAsync("You have to be an Admin to remove whitelist");
+                await msg.DeleteAsync();
+
+                return;
+            }
+
+            DBConnect dBConnect = new DBConnect();
+
+            if (hexID != "" || hexID != null)
+            {
+                var msg = await ReplyAsync("Removing Whitelist");
+
+                //DB Stuff
+                if (dBConnect.InWhitelist(hexID))
+                {
+                    if (dBConnect.UpdateWhitelist(hexID, 0))
+                    {
+                        Console.WriteLine($"{DateTime.Now.TimeOfDay}    Removed user: steam:{hexID}");
+                        await ReplyAsync($"User steam:{hexID} is now removed from whitelist");
+                        await msg.DeleteAsync();
+                    }
+                    else
+                    {
+                        await ReplyAsync($"Could not connect to DB");
+                        await msg.DeleteAsync();
+                    }
+                }
+                else
+                {
+                    await ReplyAsync($"User isn't whitelisted");
+                    await msg.DeleteAsync();
+                }
+            }
+        }
+    }
+}
---
+++ WhitelistDiscordBot/Services/DBConnect.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.IO;
+
+namespace WhitelistDiscordBot.Services
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "127.0.0.1";
+            database = "essentialsmode";
+            uid = "root";
+            password = "orebro123";
+            string connectionString = "SERVER=" + server + "; " + "DATABASE=" +
+            database + "; " + "UID=" + uid + "; " + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        //0: Cannot connect to server.
+                        break;
+
+                    case 1045:
+                        //1045: Invalid user name and/or password.
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                
+                return false;
+            }
+        }
+
+        public bool Whitelist(string hexID)
+        {
+            string query = $"INSERT INTO user_whitelist (identifier, whitelisted) VALUES('steam:{hexID}', '1')";
+
+            //open connection
+            if (OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                CloseConnection();
+
+                return true;
+            }
+            return false;
+        }
+
+        public bool InWhitelist(string hexID)
+        {
+            string query = $"SELECT * FROM user_whitelist WHERE identifier='steam:{hexID}'";
+
+            //open connection
+            if (OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    dataReader.Close();
+
+                    CloseConnection();
+
+                    return true;
+                }
+                dataReader.Close();
+
+                CloseConnection();
+                return false;
+            }
+            return false;
+        }
+
+        public bool UpdateWhitelist(string hexID, short wl)
+        {
+            string query = $"UPDATE user_whitelist SET whitelisted='{wl}' WHERE identifier='steam:{hexID}'";
+
+            //open connection
+            if (OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                CloseConnection();
+
+                return true;
+            }
+            return false;
+        }
+    }
+}
